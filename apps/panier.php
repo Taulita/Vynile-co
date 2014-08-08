ae16d498457613178d388d44e3ac3614995039ff
Taulita <taulita82@gmail.com>
2014-08-08 10:41:26 +0200

    panier

--- apps/panier.php
+++ apps/panier.php
@@ -1,3 +1,19 @@
 <?php
-require('views/panier.phtml');
-?>
+
+if(isset($_POST['panier']))
+{
+	if (!(isset($_SESSION['panier'])))
+		$_SESSION['panier']=array();
+	else
+		$_SESSION['panier'];
+
+	array_push($_SESSION['panier'], $_POST['id']);
+
+	header("Location: ".$_SERVER['HTTP_REFERER']."");
+}
+else
+{
+
+	require('views/panier.phtml');
+}
+
